835cd3dfc33e3063cea51ecdedbc08aff23301b0
naza21 <mateus.rossisantos@gmail.com>
2025-04-16 21:58:57 -0300

    adição .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIaQdEfQPWEkM4zmTNug5Mot_MkGV1rT4",
-    authDomain: "concentrapay.firebaseapp.com",
-    projectId: "concentrapay",
-    storageBucket: "concentrapay.firebasestorage.app",
-    messagingSenderId: "724864630167",
-    appId: "1:724864630167:web:58303aaf71b84bc9e618d2"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
